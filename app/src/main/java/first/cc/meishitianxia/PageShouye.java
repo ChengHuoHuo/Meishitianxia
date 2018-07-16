0a15f85c2e7269515560e818b67d52fd58cc7e85
ChengHuoHuo <442237729@qq.com>
2018-07-16 09:45:03 +0800

    Initial commit

---
+++ app/src/main/java/first/cc/meishitianxia/PageShouye.java
@@ -0,0 +1,23 @@
+package first.cc.meishitianxia;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by 44223 on 2018/7/10.
+ */
+
+public class PageShouye extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view=inflater.inflate(R.layout.vp_shouye,container, false);
+
+        return view;
+    }
+}
